b949fca7908033e9b89f744cdcf61d1d3302d4fd
Brian Merrick <brian.merrick@boomi.com>
2025-04-09 10:48:53 -0400

    adding consumer

--- pulsar.m4
+++ pulsar.m4
@@ -6,6 +6,7 @@
 # ARG_OPTIONAL_SINGLE([token],[t],[JWT Token])
 # ARG_OPTIONAL_SINGLE([producer],[p],[persistent://ACCOUNT/TOPIC/SUBSCRIBER])
 # ARG_OPTIONAL_SINGLE([consumer],[c],[persistent://ACCOUNT/TOPIC/SUBSCRIBER])
+# ARG_OPTIONAL_SINGLE([subscription],[s],[SUBSCRIBER])
 # ARG_HELP([-t TOKEN -p persistent://ACCOUNT/TOPIC/SUBSCRIBER])
 # ARGBASH_GO()
 # needed because of Argbash --> m4_ignore([
@@ -34,7 +35,6 @@ begins_with_short_option()
 # THE DEFAULTS INITIALIZATION - OPTIONALS
 _arg_token=
 _arg_producer=
-
 
 print_help()
 {
@@ -100,22 +100,51 @@ parse_commandline "$@"
 
 echo "Value of --token: $_arg_token"
 echo "Value of --producer: $_arg_producer"
+echo "Value of --consumer: $_arg_consumer"
+echo "Value of --subscription: $_arg_subscription"
 echo "print is $_arg_print"
 
-if [ -z "$_arg_token" ] || [ -z "$_arg_producer" ]
+if [ -z "$_arg_token" ]
 then
   print_help
   die
 fi
 
-for i in {1..10}
-do
-# persistent/ACCOUNT_NAME/ENVIRONMENT_ID/TOPIC	
-~/apache-pulsar/bin/pulsar-client --url pulsar+ssl://usa-east.eventstreams.boomi.com:6651 \
-  --auth-plugin "org.apache.pulsar.client.impl.auth.AuthenticationToken" \
-  --auth-params "token:$_arg_token" \
-  produce $_arg_producer \
-  -f $(pwd)/data/gps_iot.xml -n 1000 &
-done
+if [ ! -z "$_arg_producer" ] && [ ! -z "$_arg_consumer" ]
+then
+  printf 'Cannot set a producer and consumer in one execution....exiting\n'
+  print_help
+  die
+fi
+
+if [ ! -z "$_arg_producer" ]
+then
+  echo "Starting producer...."
+  for i in {1..10}
+  do
+  # persistent/ACCOUNT_NAME/ENVIRONMENT_ID/TOPIC	
+  ~/apache-pulsar/bin/pulsar-client --url pulsar+ssl://usa-east.eventstreams.boomi.com:6651 \
+    --auth-plugin "org.apache.pulsar.client.impl.auth.AuthenticationToken" \
+    --auth-params "token:$_arg_token" \
+    produce $_arg_producer \
+    -f $(pwd)/data/gps_iot.xml -n 1000 &
+  done
+fi
+
+if [ ! -z "$_arg_consumer" ]
+then
+  if [ -z "$_arg_subscription" ]
+  then
+    echo "Subscription is required for a consume operation...exiting"
+    die
+  fi
+
+  echo "Starting consumer..."
+  ~/apache-pulsar/bin/pulsar-client --url pulsar+ssl://usa-east.eventstreams.boomi.com:6651 \
+    --auth-plugin "org.apache.pulsar.client.impl.auth.AuthenticationToken" \
+    --auth-params "token:$_arg_token" \
+    consume -s "$_arg_subscription" "$_arg_consumer" \
+    -n 0 
+fi
 
 # ] <-- needed because of Argbash
--- pulsar.sh
+++ pulsar.sh
@@ -6,6 +6,7 @@
 # ARG_OPTIONAL_SINGLE([token],[t],[JWT Token])
 # ARG_OPTIONAL_SINGLE([producer],[p],[persistent://ACCOUNT/TOPIC/SUBSCRIBER])
 # ARG_OPTIONAL_SINGLE([consumer],[c],[persistent://ACCOUNT/TOPIC/SUBSCRIBER])
+# ARG_OPTIONAL_SINGLE([subscription],[s],[SUBSCRIBER])
 # ARG_HELP([-t TOKEN -p persistent://ACCOUNT/TOPIC/SUBSCRIBER])
 # ARGBASH_GO()
 # needed because of Argbash --> m4_ignore([
@@ -25,7 +26,7 @@ die()
 
 begins_with_short_option()
 {
-	local first_option all_short_options='tpch'
+	local first_option all_short_options='tpcsh'
 	first_option="${1:0:1}"
 	test "$all_short_options" = "${all_short_options/$first_option/}" && return 1 || return 0
 }
@@ -34,15 +35,17 @@ begins_with_short_option()
 _arg_token=
 _arg_producer=
 _arg_consumer=
+_arg_subscription=
 
 
 print_help()
 {
 	printf '%s\n' "-t TOKEN -p persistent://ACCOUNT/TOPIC/SUBSCRIBER"
-	printf 'Usage: %s [-t|--token <arg>] [-p|--producer <arg>] [-c|--consumer <arg>] [-h|--help]\n' "$0"
+	printf 'Usage: %s [-t|--token <arg>] [-p|--producer <arg>] [-c|--consumer <arg>] [-s|--subscription <arg>] [-h|--help]\n' "$0"
 	printf '\t%s\n' "-t, --token: JWT Token (no default)"
 	printf '\t%s\n' "-p, --producer: persistent://ACCOUNT/TOPIC/SUBSCRIBER (no default)"
 	printf '\t%s\n' "-c, --consumer: persistent://ACCOUNT/TOPIC/SUBSCRIBER (no default)"
+	printf '\t%s\n' "-s, --subscription: SUBSCRIBER (no default)"
 	printf '\t%s\n' "-h, --help: Prints help"
 }
 
@@ -86,6 +89,17 @@ parse_commandline()
 			-c*)
 				_arg_consumer="${_key##-c}"
 				;;
+			-s|--subscription)
+				test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
+				_arg_subscription="$2"
+				shift
+				;;
+			--subscription=*)
+				_arg_subscription="${_key##--subscription=}"
+				;;
+			-s*)
+				_arg_subscription="${_key##-s}"
+				;;
 			-h|--help)
 				print_help
 				exit 0
@@ -112,22 +126,51 @@ parse_commandline "$@"
 
 echo "Value of --token: $_arg_token"
 echo "Value of --producer: $_arg_producer"
+echo "Value of --consumer: $_arg_consumer"
+echo "Value of --subscription: $_arg_subscription"
 echo "print is $_arg_print"
 
-if [ -z "$_arg_token" ] || [ -z "$_arg_producer" ]
+if [ -z "$_arg_token" ]
 then
   print_help
   die
 fi
 
-for i in {1..10}
-do
-# persistent/ACCOUNT_NAME/ENVIRONMENT_ID/TOPIC
-~/apache-pulsar/bin/pulsar-client --url pulsar+ssl://usa-east.eventstreams.boomi.com:6651 \
-  --auth-plugin "org.apache.pulsar.client.impl.auth.AuthenticationToken" \
-  --auth-params "token:$_arg_token" \
-  produce $_arg_producer \
-  -f $(pwd)/data/gps_iot.xml -n 1000 &
-done
+if [ ! -z "$_arg_producer" ] && [ ! -z "$_arg_consumer" ]
+then
+  printf 'Cannot set a producer and consumer in one execution....exiting\n'
+  print_help
+  die
+fi
+
+if [ ! -z "$_arg_producer" ]
+then
+  echo "Starting producer...."
+  for i in {1..10}
+  do
+  # persistent/ACCOUNT_NAME/ENVIRONMENT_ID/TOPIC
+  ~/apache-pulsar/bin/pulsar-client --url pulsar+ssl://usa-east.eventstreams.boomi.com:6651 \
+    --auth-plugin "org.apache.pulsar.client.impl.auth.AuthenticationToken" \
+    --auth-params "token:$_arg_token" \
+    produce $_arg_producer \
+    -f $(pwd)/data/gps_iot.xml -n 1000 &
+  done
+fi
+
+if [ ! -z "$_arg_consumer" ]
+then
+  if [ -z "$_arg_subscription" ]
+  then
+    echo "Subscription is required for a consume operation...exiting"
+    die
+  fi
+
+  echo "Starting consumer..."
+  ~/apache-pulsar/bin/pulsar-client --url pulsar+ssl://usa-east.eventstreams.boomi.com:6651 \
+    --auth-plugin "org.apache.pulsar.client.impl.auth.AuthenticationToken" \
+    --auth-params "token:$_arg_token" \
+    consume -s "$_arg_subscription" "$_arg_consumer" \
+    -n 0
+fi
 
 # ] <-- needed because of Argbash
